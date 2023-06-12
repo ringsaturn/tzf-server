48d86b8bac17c52556387e817f2a371c9ac5e12f
ringsaturn <ringsaturn.me@gmail.com>
2023-06-13 00:57:45 +0800

    add redis protocol server (#79)

--- handler/api.go
+++ handler/api.go
@@ -3,9 +3,12 @@ package handler
 import (
 	"context"
 	"net/http"
+	"strconv"
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/pkg/errors"
+	"github.com/tidwall/redcon"
 )
 
 type LocationRequest struct {
@@ -69,4 +72,52 @@ func GetTimezoneShape(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 	c.JSON(http.StatusOK, shape)
+}
+
+func parseCoordinates(cmd redcon.Command) (float64, float64, error) {
+	if len(cmd.Args) != 3 {
+		return 0, 0, errors.New("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
+	}
+	lng, err := strconv.ParseFloat(string(cmd.Args[1]), 64)
+	if err != nil {
+		return 0, 0, err
+	}
+	lat, err := strconv.ParseFloat(string(cmd.Args[2]), 64)
+	if err != nil {
+		return 0, 0, err
+	}
+	return lng, lat, nil
+}
+
+func RedisGetTZCmd(conn redcon.Conn, cmd redcon.Command) {
+	lng, lat, err := parseCoordinates(cmd)
+	if err != nil {
+		conn.WriteError(err.Error())
+		return
+	}
+
+	timezone_name := finder.GetTimezoneName(lng, lat)
+	if timezone_name == "" {
+		conn.WriteError("no tz found")
+		return
+	}
+	conn.WriteString(timezone_name)
+}
+
+func RedisGetTZsCmd(conn redcon.Conn, cmd redcon.Command) {
+	lng, lat, err := parseCoordinates(cmd)
+	if err != nil {
+		conn.WriteError(err.Error())
+		return
+	}
+
+	timezone_names, err := finder.GetTimezoneNames(lng, lat)
+	if err != nil {
+		conn.WriteError("no tz found")
+		return
+	}
+	conn.WriteArray(len(timezone_names))
+	for _, name := range timezone_names {
+		conn.WriteBulkString(name)
+	}
 }
