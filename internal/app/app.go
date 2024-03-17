b015e13894cac1705b880cba3f87d5989dcdca08
ringsaturn <ringsaturn.me@gmail.com>
2024-03-17 09:16:37 +0800

    update

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,10 +1,16 @@
 package app
 
 import (
+	"context"
+	"net/http"
+	"time"
+
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/google/wire"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/ringsaturn/tzf-server/internal/config"
 	"github.com/ringsaturn/tzf-server/internal/redisserver"
+	"golang.org/x/sync/errgroup"
 )
 
 type App struct {
@@ -27,6 +33,38 @@ func (a *App) StartHTTPServer() error {
 
 func (a *App) StartRedisServer() error {
 	return a.RedisServer.StartRedisServer(a.cfg.RedisAddr)
+}
+
+func (a *App) StartPrometheus(ctx context.Context) error {
+	mux := http.NewServeMux()
+	mux.Handle(a.cfg.PrometheusPath, promhttp.Handler())
+
+	app := &http.Server{
+		Addr:           a.cfg.PrometheusHostPorts,
+		ReadTimeout:    30 * time.Second,
+		WriteTimeout:   30 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+		Handler:        mux,
+	}
+
+	go func() {
+		<-ctx.Done()
+		_ = app.Shutdown(context.Background())
+	}()
+
+	return app.ListenAndServe()
+}
+
+func (a *App) Start(ctx context.Context) error {
+	g, gCtx := errgroup.WithContext(ctx)
+
+	g.Go(a.StartHTTPServer)
+	g.Go(a.StartRedisServer)
+	g.Go(func() error {
+		return a.StartPrometheus(gCtx)
+	})
+
+	return g.Wait()
 }
 
 var ProviderSet = wire.NewSet(NewApp)
--- main.go
+++ main.go
@@ -2,116 +2,15 @@ package main
 
 import (
 	"context"
-	_ "embed"
-	"flag"
-	"net/http"
-	"os"
-	"time"
-
-	"github.com/cloudwego/hertz/pkg/app/server"
-	prometheus "github.com/hertz-contrib/monitor-prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/ringsaturn/tzf-server/internal"
-	tzfserver "github.com/ringsaturn/tzf-server/internal/server"
-	v1 "github.com/ringsaturn/tzf-server/tzf/v1"
-	"go.uber.org/zap"
-	"golang.org/x/sync/errgroup"
-)
-
-var (
-	finderType                  = flag.Int("type", 0, "which finder to use Polygon(0) or Fuzzy(1)")
-	dataPath                    = flag.String("path", "", "custom data")
-	httpAddr                    = flag.String("http-addr", "0.0.0.0:8080", "HTTP Host&Port")
-	redisAddr                   = flag.String("redis-addr", "localhost:6380", "Redis Server Host&Port")
-	prometheusHostPorts         = flag.String("prometheus-host-port", "0.0.0.0:2112", "Prometheus Host&Port")
-	prometheusPath              = flag.String("prometheus-path", "/metrics", "Prometheus Path")
-	hertzPrometheusHostPorts    = flag.String("hertz-prometheus-host-port", "0.0.0.0:8090", "Hertz Prometheus Host&Port")
-	hertzPrometheusPath         = flag.String("hertz-prometheus-path", "/hertz", "Hertz Prometheus Path")
-	prometheusEnableGoCollector = flag.Bool("prometheus-enable-go-coll", true, "Enable Go Collector")
-	disablePrintRoute           = flag.Bool("disable-print-route", false, "Disable Print Route")
+	"fmt"
+	"log"
 )
 
 func main() {
-	flag.Parse()
-
-	logger := internal.Loggger
-	logger.Info("starting")
-	logger.Sugar().Infow("Get config",
-		"type", *finderType,
-		"path", *dataPath,
-		"http-addr", *httpAddr,
-		"redis-addr", *redisAddr,
-		"prometheus-host-port", *prometheusHostPorts,
-		"prometheus-path", prometheusPath,
-		"prometheus-enable-go-coll", *prometheusEnableGoCollector,
-		"disable-print-route", *disablePrintRoute,
-	)
-
-	switch *finderType {
-	case 0:
-		logger.Debug("Will use Polygon data")
-	case 1:
-		logger.Debug("Will use Fuzzy data")
-	default:
-		logger.Error("Unknown method, quit.")
-		return
-	}
-
-	if *dataPath == "" {
-		logger.Debug("Will use built-in tzf-rel data")
-	} else {
-		logger.Debug("Will use custom data")
-	}
-
-	hertz := tzfserver.Setup(
-		&tzfserver.SetupFinderOptions{
-			FinderType:     tzfserver.FinderType((*finderType)),
-			CustomDataPath: *dataPath,
-		},
-		server.WithDisablePrintRoute(*disablePrintRoute),
-		server.WithHostPorts(*httpAddr),
-		server.WithTracer(
-			prometheus.NewServerTracer(
-				*hertzPrometheusHostPorts,
-				*hertzPrometheusPath,
-				prometheus.WithEnableGoCollector(*prometheusEnableGoCollector),
-			),
-		),
-	)
-
-	v1.BindDefaultSwagger(hertz)
-
-	rootCtx := context.Background()
-
-	g, gCtx := errgroup.WithContext(rootCtx)
-
-	g.Go(hertz.Run)
-
-	g.Go(func() error {
-		mux := http.NewServeMux()
-		mux.Handle(*prometheusPath, promhttp.Handler())
-
-		app := &http.Server{
-			Addr:           *prometheusHostPorts,
-			ReadTimeout:    30 * time.Second,
-			WriteTimeout:   30 * time.Second,
-			MaxHeaderBytes: 1 << 20,
-			Handler:        mux,
-		}
-
-		go func() {
-			<-gCtx.Done()
-			_ = app.Shutdown(context.Background())
-		}()
-
-		return app.ListenAndServe()
-	})
-
-	// g.Go(func() error { return tzfserver.StartRedisServer(*redisAddr) })
-
-	err := g.Wait()
+	ctx := context.Background()
+	a, err := newApp(ctx)
 	if err != nil {
-		logger.Error("error", zap.Error(err))
-		os.Exit(1)
+		log.Fatal(err)
 	}
+	fmt.Println(a.Start(ctx))
 }
